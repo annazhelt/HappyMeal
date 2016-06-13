c88796be953dd4277891bc60d2b234e1492789e6
Song <Vittoria@SongHuandeMacBook-Pro.local>
2016-06-13 03:52:33 -0700

    add entities of order,review,customer,user and member in src

---
+++ src/happymeal/entity/customer.java
@@ -0,0 +1,29 @@
+package happymeal.entity;
+
+import java.util.List;
+import static java.lang.System.out;
+
+/**
+ * created by Vittoria on 2016-06-13
+ */
+
+public class Customer  extends user{
+    private String cname;
+    private int phone;
+    
+    public int getPhone() {
+        return this.phone;
+    }
+    
+    public String getCname(){
+        return this.cname;
+    }
+    
+    public void setPhone(int phoneNum){
+        this.phone = phoneNum;
+    }
+    
+    public void setCname(String name) {
+        this.cname = name;
+    }
+}
