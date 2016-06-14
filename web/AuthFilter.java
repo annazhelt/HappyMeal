05157d611e791ec1f91c6fe5d117ba9278b89a7b
Anna <anna@Annas-MacBook-Air.local>
2016-06-14 15:15:24 -0700

    not logged in users cannot access any of the functionality anymore

---
+++ web/AuthFilter.java
@@ -0,0 +1,39 @@
+package web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by anna on 2016-06-14.
+ */
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+        String loginURI = request.getContextPath() + "/login";
+
+        boolean loggedIn = session != null && session.getAttribute("user") != null;
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            chain.doFilter(request, response);
+        } else {
+            //req.setAttribute("message", "Need to login.");
+            response.sendRedirect(loginURI);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
